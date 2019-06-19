4ec460182badb7e9466825b3af2231807a183ff7
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2019-06-19 22:58:14 +0900

    Added navigation and navbar functionality for desktop and mobile

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,24 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Icon } from "antd";
 
 import styles from "./header.module.scss";
 
 const Header = () => {
+  const [visibleMenu, setVisibleMenu] = useState(false);
+
   return (
-    <div className={styles.menu}>
-      <Link to="/">
-        <span style={{ fontSize: "1rem" }}>{"\u{1208D}"}</span>
-      </Link>
-      <Link to="/dictionary">
-        <span>Dictionary</span>
-      </Link>
-      <Link to="/conjugator">
-        <span>Conjugator</span>
-      </Link>
-      <Link to="/flashcardgame">
-        <span>Game</span>
-      </Link>
-    </div>
+    <>
+      <nav className={styles.menu}>
+        <ul>
+          <li>
+            <Link to="/">
+              <span style={{ fontSize: "1rem" }}>{"\u{1208D}"}</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/dictionary">
+              <span>Dictionary</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/conjugator">
+              <span>Conjugator</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/flashcardgame">
+              <span>Game</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav
+        className={styles.menuMobile}
+        style={
+          visibleMenu
+            ? { backgroundColor: "#fff7e6" }
+            : { backgroundColor: "#ffd591" }
+        }
+      >
+        <Icon
+          type="menu"
+          className={styles.burger}
+          onClick={() => setVisibleMenu(!visibleMenu)}
+        />
+        <ul
+          style={visibleMenu ? { display: "block" } : { display: "none" }}
+          className={styles.menuMobileNav}
+        >
+          <li onClick={() => setVisibleMenu(false)}>
+            <Link to="/">
+              <span style={{ fontSize: "1rem" }}>{"\u{1208D}"}</span>
+            </Link>
+          </li>
+          <li onClick={() => setVisibleMenu(false)}>
+            <Link to="/dictionary">
+              <span>Dictionary</span>
+            </Link>
+          </li>
+          <li onClick={() => setVisibleMenu(false)}>
+            <Link to="/conjugator">
+              <span>Conjugator</span>
+            </Link>
+          </li>
+          <li onClick={() => setVisibleMenu(false)}>
+            <Link to="/flashcardgame">
+              <span>Game</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
